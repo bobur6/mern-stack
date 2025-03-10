485241caccd206b1d5e7db49d1ed0e31dda82e12
Bobur <boburforopen@gmail.com>
2025-03-10 09:29:01 +0500

    feat: проверка работы commitlint

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
-import path from "path";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import cors from 'cors';
 
-import { connectDB } from "./config/db.js";
+import { connectDB } from './config/db.js';
 
-import productRoutes from "./routes/product.route.js";
+import productRoutes from './routes/product.route.js';
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: '../.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,22 +17,22 @@ const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.use("/api/products", productRoutes);
+app.use('/api/products', productRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
 }
 
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({ success: false, message: "Something went wrong!" });
+app.use((err, req, res, _next) => {
+  console.error(err.stack);
+  res.status(500).json({ success: false, message: 'Something went wrong!' });
 });
 
 app.listen(PORT, () => {
-	console.log("MONGO_URI:", process.env.MONGO_URI); //временно
-	connectDB();
-	console.log("Server started at http://localhost:" + PORT);
+  console.log('MONGO_URI:', process.env.MONGO_URI); //временно
+  connectDB();
+  console.log('Server started at http://localhost:' + PORT);
 });
